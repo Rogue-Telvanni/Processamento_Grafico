d1bf0cb9bd305eb9c0a600444a776b816c627b8f
Rogue-Telvanni <joaocopini11@outlook.com>
2024-10-04 01:28:34 -0300

    adição do scene manager

---
+++ Semana_6_Iluminacao/object.cpp
@@ -0,0 +1,13 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// GLAD
+#include <glad/glad.h>
+
+struct Object
+{
+	GLuint VAO; //Índice do buffer de geometria
+	int nVertices; //nro de vértices
+	glm::mat4 model; //matriz de transformações do objeto
+};
